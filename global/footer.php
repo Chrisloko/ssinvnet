6c80c5dafbf396a4b7079013832924e03e0ada63
Rod <rodrigocesarc@hotmail.com>
2020-05-29 16:26:15 -0500

    added php

---
+++ global/footer.php
@@ -0,0 +1,162 @@
+
+
+    <footer class="" style="background-color: #000102">
+     
+        <div class="footer-top">
+            <div class="container">
+                <div class="row" style="justify-content: center">
+                    <div class="flex:0 0 100%" >
+                        <ul class="nav navbar-nav navbar-right" data-in="#" data-out="#">
+							<li class="dropdown">
+								<a href="/" class="active" >HOME</a>
+
+							</li>
+							<li class="">
+								<a href="#" class="" data-toggle="">ABOUT</a>
+							</li>
+							<li>
+								<a href="#" class="active" data-toggle="dropdown" >CAPABILITIES</a>
+							</li>
+							<li class="">
+								<a href="#" class="">FAQS</a>   
+							</li>
+							<li class="">
+								<a href="#" class="">NEWS</a>   
+							</li>
+							<li>
+								<a href="#">CONTACT</a>
+							</li>
+						</ul>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <div style="position: relative">
+			<img src="assets/images/footer-thing.jpg" class="footer-thing-resp" style="position: absolute; left: 0; top: 12%" alt="">
+        <div class="container">
+            <div class="row">
+                <div class="f-items" style="padding: 0 0 120px">
+
+                
+                    <div class="col-md-4 equal-height item resp-footer-social">
+                        <div class="f-item">
+							<a href="callto:6195555888">Call Us: (619) 555-5888</a>
+							<div class="social">
+                                <ul>
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
+                                </ul>
+                            </div>
+                            
+                          
+                            
+                        </div>
+                    </div>
+              
+                    <div class="col-md-4 equal-height item" style="text-align: center">
+                        <div class="f-item link">
+							<a href="/"><img src="assets/images/logo-top.svg" style="width: 300px; height: 69px; margin-bottom: 50px" alt=""></a>
+                            <h4 style="color:#2c86c8"><i>Our Pledge to you</i></h4>
+                            <p>At BizSecure we stive for the highest standards of service, integrity, and quality; to uphold values that serve you, your patrons, and your mission;  to be reliable and communicative, and to set expectations that we can deliver upon regardless of the task at hand, so our relationship will endure beyond today, beyond the project, and beyond your department.</p>
+                        </div>
+                    </div>
+              
+                    <div class="col-md-4 contact-form equal-height item">
+                        
+                        <p style="margin-bottom: 25px">
+                            The Who’s Who Of Business intelligence
+                        </p>
+						<p>REQUEST A CALL BACK</p>
+                        <form action="assets/mail/contact.php" method="POST" class="contact-form">
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="form-group">
+                                        <input class="form-control" id="name" name="name" placeholder="Name" type="text">
+                                        <span class="alert-error"></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <input class="form-control" id="email" name="email" placeholder="Email*" type="email">
+                                        <span class="alert-error"></span>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <input class="form-control" id="phone" name="phone" placeholder="Phone" type="text">
+                                        <span class="alert-error"></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="form-group comments">
+                                        <textarea class="form-control" id="comments" name="comments" placeholder="Tell Us About Project *"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <button type="submit" name="submit" id="submit">
+                                        Send Message <i class="fa fa-paper-plane"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <!-- Alert Message -->
+                            <div class="col-md-12 alert-notification">
+                                <div id="message" class="alert-msg"></div>
+                            </div>
+                        </form>
+                    </div>
+     
+                </div>
+            </div>
+        </div>
+		</div> 
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <p>&copy; Copyright 2020. All Rights Reserved by <a href="#">Bizsecure</a></p>
+                    </div>
+                    <div class="col-md-6 text-right link">
+                        <ul>
+                            <li>
+                                <a href="#">Privacy Policy</a>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+      
+    </footer>
+    <!-- End Footer -->
+
+    <!-- jQuery Frameworks
+    ============================================= -->
+    <script src="assets/js/jquery-1.12.4.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/equal-height.min.js"></script>
+    <script src="assets/js/jquery.appear.js"></script>
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/modernizr.custom.13711.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/progress-bar.min.js"></script>
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/js/count-to.js"></script>
+    <script src="assets/js/YTPlayer.min.js"></script>
+    <script src="assets/js/bootsnav.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+</html>
